ea4fa6c66e06a23653021a54f1b899bc36ff8e0c
s4171055 <s4171055@student.rmit.edu.au>
2026-05-04 13:58:49 +1000

    task one comments

---
+++ task_1.py
@@ -0,0 +1,52 @@
+'''
+Task 1: Digital Signature-Based Record Authentication (10 Marks)
+Each inventory node generates a new inventory record representing a recent item update. Before the record
+is broadcast to the distributed inventory system, the originating node must apply a digital signature to ensure
+the authenticity and integrity of the submitted data.
+
+What You Need to Do
+1. Initialise the cryptographic parameters required for digital signature operations using the values
+provided in the List of Keys document.
+
+2. Derive any additional key components required for the digital signature process from the provided
+parameters, and ensure all required values are explicitly defined in your code.
+
+3. Implement a mechanism that enables an inventory node to digitally sign a newly generated inventory
+record prior to submission.
+
+4. Implement a verification process that allows other inventory nodes to validate the authenticity and
+integrity of the received record before it proceeds to the consensus stage.
+
+5. In your report, explain how digital signatures contribute to secure record submission in a distributed
+inventory environment.
+'''
+
+'''
+1. Initialise the cryptographic parameters required for digital signature operations using the values
+provided in the List of Keys document.
+
+get key values (A, B, C, D) from A2_Blockchain/A2_ListOfKeys.txt and assign them to variables:
+in_A_p, in_A_q, in_A_e
+in_B_p, in_B_q, in_B_e
+in_C_p, in_C_q, in_C_e
+in_D_p, in_D_q, in_D_e
+
+Part 1
+Inventory A
+p = 1210613765735147311106936311866593978079938707
+q = 1247842850282035753615951347964437248190231863
+e = 815459040813953176289801
+Inventory B
+p = 787435686772982288169641922308628444877260947
+q = 1325305233886096053310340418467385397239375379
+e = 692450682143089563609787
+Inventory C
+p = 1014247300991039444864201518275018240361205111
+q = 904030450302158058469475048755214591704639633
+e = 1158749422015035388438057
+Inventory D
+p = 1287737200891425621338551020762858710281638317
+q = 1330909125725073469794953234151525201084537607
+e = 33981230465225879849295979
+
+'''
